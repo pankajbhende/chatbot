f20512851026c730f98c891dcd317a41697feffb
niishiipatidar <91723222+niishiipatidar@users.noreply.github.com>
2025-07-25 14:05:08 +0530

    Fixed Minority groups

--- echo-mind-chatbot/src/components/ChatPage.tsx
+++ echo-mind-chatbot/src/components/ChatPage.tsx
@@ -14,7 +14,7 @@ function ChatPage() {
   const [age, setAge] = useState<number>();
   const [salary, setSalary] = useState<number>();
   const [minority, setMinority] = useState<boolean>(false);
-  const [isSpeaking, setIsSpeaking] = useState<boolean>(false);
+  const [isSpeaking, setIsSpeaking] = useState<boolean>();
   const [showWelcome, setShowWelcome] = useState(true);
 const [fadeWelcome, setFadeWelcome] = useState(false);
 const [loading, setLoading] = useState(false);
